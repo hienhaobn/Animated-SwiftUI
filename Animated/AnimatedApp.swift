c65ea2f976eba2bac2bfa54bec88e1f395e62f5b
Hien Hao <hienhao021198@gmail.com>
2025-08-16 22:52:52 +0700

    Initial Commit

---
+++ Animated/AnimatedApp.swift
@@ -0,0 +1,17 @@
+//
+//  AnimatedApp.swift
+//  Animated
+//
+//  Created by Hiển Hảo on 16/08/2025.
+//
+
+import SwiftUI
+
+@main
+struct AnimatedApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ AnimatedUITests/AnimatedUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  AnimatedUITestsLaunchTests.swift
+//  AnimatedUITests
+//
+//  Created by Hiển Hảo on 16/08/2025.
+//
+
+import XCTest
+
+final class AnimatedUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
