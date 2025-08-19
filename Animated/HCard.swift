8d8f7be3b79841cfbc7f334447bffc8b0e2735aa
Hien Hao <hienhao021198@gmail.com>
2025-08-20 06:48:01 +0700

    add side menu, chat screen

---
+++ Animated/HCard.swift
@@ -0,0 +1,18 @@
+//
+//  HCard.swift
+//  Animated
+//
+//  Created by Hiển Hảo on 19/08/2025.
+//
+
+import SwiftUI
+
+struct HCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    HCard()
+}
---
+++ Animated/VCard.swift
@@ -0,0 +1,18 @@
+//
+//  VCard.swift
+//  Animated
+//
+//  Created by Hiển Hảo on 19/08/2025.
+//
+
+import SwiftUI
+
+struct VCard: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    VCard()
+}
